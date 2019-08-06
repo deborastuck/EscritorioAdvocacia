1ace2b4c7aa60ef7290e6692cc55dca6e4e25de1
deborastuck <38276765+deborastuck@users.noreply.github.com>
2019-08-06 15:17:19 -0300

    Add files via upload

---
+++ EscritorioAdvocacia/Data/EscritorioAdvocaciaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscritorioAdvocacia.Models
+{
+    public class EscritorioAdvocaciaContext : DbContext
+    {
+        public EscritorioAdvocaciaContext (DbContextOptions<EscritorioAdvocaciaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EscritorioAdvocacia.Models.Cliente> Cliente { get; set; }
+        public DbSet<EscritorioAdvocacia.Models.Processo> Processo { get; set; }
+    }
+}
---
+++ EscritorioAdvocacia/Migrations/20190805004130_EscritorioProcesso.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscritorioAdvocacia.Migrations
+{
+    public partial class EscritorioProcesso : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Processo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Numero = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Valor = table.Column<double>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Processo_Cliente_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Processo_ClienteId",
+                table: "Processo",
+                column: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Processo");
+        }
+    }
+}
